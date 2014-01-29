978bcaae7446137701e80379cd1f4fbcdd1099fd
Luciano Ramalho <luciano@ramalho.org>
2014-01-29 17:50:54 -0200

    exemplo blink com pyun

---
+++ blink.py
@@ -0,0 +1,12 @@
+from time import sleep
+
+from pyun import *
+
+yun = YunBridge('192.168.2.9')
+yun.pinMode(13, OUTPUT)
+
+while True:
+    yun.digitalWrite(13, 1)
+    yun.delay(1000)
+    yun.digitalWrite(13, 0)
+    yun.delay(1000)
